994deb9c77691f17e47ea921ed95ac3cf250cce6
Oluwaseun <oluwaseunus@gmail.com>
2021-09-24 11:44:32 +0100

    chore: remove react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages';
 import CityPage from './pages/city';
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/:city' component={CityPage} />
-        </Switch>
-      </QueryClientProvider>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/:city' component={CityPage} />
+      </Switch>
     </BrowserRouter>
   );
 }
